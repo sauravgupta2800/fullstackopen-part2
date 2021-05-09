e959f0ccc7823819458592e7459eefa57fe4f0cb
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-09 15:10:24 +0530

    2.9*: The Phonebook Step4

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 123455 },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchKey, setSearchKey] = useState("");
 
   const handleAddName = (event) => {
     event.preventDefault();
@@ -24,9 +28,22 @@ const App = () => {
     setNewNumber("");
   };
 
+  const filteredPersons = () => {
+    return searchKey
+      ? persons.filter((filter) => filter.name.includes(searchKey))
+      : persons;
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown up with:
+        <input
+          value={searchKey}
+          onChange={(event) => setSearchKey(event.target.value)}
+        />
+      </div>
       <form onSubmit={handleAddName}>
         <div>
           name:
@@ -48,7 +65,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons().map((person) => (
         <div key={person.name}>
           {person.name}
           {person.number}
